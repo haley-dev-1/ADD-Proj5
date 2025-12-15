42421fa7dc4479fb4de8946701b771dc520dace6
haley lind <hlind.engineering@gmail.com>
2025-12-15 17:37:38 -0500

    added no ops after each instruction

--- lab_jb/square_root.asm
+++ lab_jb/square_root.asm
@@ -6,56 +6,104 @@
 
 main:
     csrrw   x8,  0xf00, x0      # x8  = CSR[0xf00] (switch input)
+    addi    x0,  x0,  x0
+    
     slli    x8,  x8, 14         # fixed-point scale: value << 14
+    addi    x0,  x0,  x0
+    
     add     x10, x0,  x8        # a0 = input (for sqrt)
+    addi    x0,  x0,  x0
 
     jal     x1,  sqrt           # call sqrt(a0) → a0
+    addi    x0,  x0,  x0
 
     lui     x5,  0x18           # x5 = 0x18_000
+    addi    x0,  x0,  x0
+
     addi    x5,  x5, 1696       # x5 = 0x18_6a0 = 100000 (magic constant)
+    addi    x0,  x0,  x0
+
     mul     x6,  x10, x5        # x6 = a0 * 100000
+    addi    x0,  x0,  x0
+
     srli    x9,  x6, 14         # x9 = (a0 * 100000) >> 14
+    addi    x0,  x0,  x0
+
     mulhu   x18, x5,  x10       # high part of a0*100000
+    addi    x0,  x0,  x0
+
     slli    x18, x18, 18        # shift high part
+    addi    x0,  x0,  x0
+
     or      x8,  x18, x9        # x8 = combined fixed-point result
+    addi    x0,  x0,  x0
 
     jal     x1,  bin_to_bcd     # bin_to_bcd(x8) → x12
+    addi    x0,  x0,  x0
 
     csrrw   x0,  0xf02, x12     # write BCD to display CSR 0xf02
+    addi    x0,  x0,  x0
 
     jal     x0,  main           # j main (infinite loop)
+    addi    x0,  x0,  x0
 
 # square root program below with loop to iteratively go with binary search to convert
 sqrt:
     add     x8,  x0,  x10       # x8  = input value
+    addi    x0,  x0,  x0
+
     addi    x9,  x0,  0         # x9  = 0 (initial guess)
+    addi    x0,  x0,  x0
+
     addi    x18, x0,  1         # x18 = 1
+    addi    x0,  x0,  x0
+
     slli    x18, x18, 22        # x18 = 1 << 22 (initial step size)
+    addi    x0,  x0,  x0
 
 sqrt_loop:
     beq     x18, x0,  sqrt_exit # if step == 0 → done
+    addi    x0,  x0,  x0
 
     add     x19, x9,  x18       # trial = guess + step
+    addi    x0,  x0,  x0
+    
     mul     x5,  x19, x19       # low  part of trial^2
+    addi    x0,  x0,  x0
+    
     mulhu   x6,  x19, x19       # high part of trial^2
+    addi    x0,  x0,  x0
 
     slli    x7,  x8,  14        # some scaled version of input
+    addi    x0,  x0,  x0
+
     srli    x28, x8,  18        # another scaled version of input
+    addi    x0,  x0,  x0
 
     bltu    x28, x6,  sqrt_step # if x28 < high(trial^2) → don't accept trial
+    addi    x0,  x0,  x0
+
     bltu    x6,  x28, sqrt_accept
+    addi    x0,  x0,  x0
                                 # else if high(trial^2) < x28 → accept trial
     bltu    x7,  x5,  sqrt_step # else if scaled input < low(trial^2) → don't accept
+    addi    x0,  x0,  x0
 
 sqrt_accept:
     add     x9,  x0,  x19       # guess = trial
+    addi    x0,  x0,  x0
 
 sqrt_step:
     srli    x18, x18, 1         # step >>= 1
+    addi    x0,  x0,  x0
+
     jal     x0,  sqrt_loop      # j sqrt_loop
+    addi    x0,  x0,  x0
 
 sqrt_exit:
     add     x10, x0,  x9        # a0 = guess
+    addi    x0,  x0,  x0
+
     ret                         
 
 
